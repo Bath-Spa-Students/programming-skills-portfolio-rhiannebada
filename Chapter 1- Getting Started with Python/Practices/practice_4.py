745eb16c8269ed79a7beb22fb308eb85b1225592
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-02 12:00:45 +0400

    Practice 4.py

---
+++ Chapter 1- Getting Started with Python/Practices/practice_4.py
@@ -0,0 +1,5 @@
+#Compute the area of triangle
+b=int(input ("Enter the measurement of base: "))
+h=int(input ("Enter the measurement of height: "))
+area = b*h/2
+print ("Area of the triangle= ", area , "square unit")
