00080d3c7359c2cbf64c6c3168f7902d8905ea60
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-02 12:20:37 +0400

    Practice 3

--- Chapter 1- Getting Started with Python/Practices/practice_3.py
+++ Chapter 1- Getting Started with Python/Practices/practice_3.py
@@ -1,3 +1,3 @@
 #You need to make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate line followed by a dot
 for i in range (9):
-    print (str(i+1))
+    print (str(i+1)+".") 
