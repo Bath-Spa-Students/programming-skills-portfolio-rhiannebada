c4900e3e57dac9ae85190d63025e3272db94565e
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-02 11:39:17 +0400

    Practice 2.py

---
+++ Chapter 1- Getting Started with Python/Practices/practice_2.py
@@ -0,0 +1,2 @@
+#Create a program to output "hi" 50 times, without any separator
+print ("hi" * 50)
---
+++ Chapter 1- Getting Started with Python/Practices/practice_3.py
@@ -0,0 +1,15 @@
+#You need to make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate line followed by a dot
+for i in range (9):
+    print (str(i+1))
+
+#or
+
+print ("""1
+2
+3
+4
+5
+6
+7
+8
+9""")
