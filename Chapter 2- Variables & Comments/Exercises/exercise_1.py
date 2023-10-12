3421fe04ef459262080160cdfdc022e2d42aa06e
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-12 12:38:25 +0400

    Exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise_1.py
@@ -0,0 +1,7 @@
+## Exercise 1: Assign a message to a variable, and print that message.
+##Then change the value of the variable to a new message, and print the new message.
+
+message = "Code Lab 1 is a challenging and interesting class for me."
+print (message)
+message = "I like the module."
+print (message)
