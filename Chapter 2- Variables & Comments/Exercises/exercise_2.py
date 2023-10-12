6971776bd839a166d054ef5c8f57e741be939694
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-12 12:38:55 +0400

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise_2.py
@@ -0,0 +1,4 @@
+## Exercise 2: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+##Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.
+
+print ('Mahatma Gandhi once said, "You must be the change you wish to see in the world."')
