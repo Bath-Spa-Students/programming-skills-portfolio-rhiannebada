f2108c7f498a124f5f058c6ef14b77a7ffcf7945
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-23 22:22:50 +0400

    Exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise_7.py
@@ -0,0 +1,36 @@
+## Exercise 7: Seeing the World 
+
+print ("LIST:")
+
+places = ['South Korea', 'Palawan', 'Japan', 'Maldives', 'Switzerland']
+
+print ("\nOriginal order:")
+print (places)
+
+print ("\nAlphabetical order using sorted():")
+print (sorted(places))
+
+print ("\nOriginal order:")
+print (places)
+
+print ("\nReversed Alphabetical order using sorted():")
+print (sorted(places, reverse=True))
+
+print ("\nOriginal order:")
+print (places)
+
+print ("\nReversed order using reverse():")
+places.reverse()
+print(places)
+
+print ("\nOriginal order using reverse():")
+places.reverse()
+print (places)
+
+print ("\nSorted/Alphabetical order using sort():")
+places.sort()
+print (places)
+
+print ("\nReversed Alphabetical order using sort():")
+places.sort(reverse=True)
+print (places)
