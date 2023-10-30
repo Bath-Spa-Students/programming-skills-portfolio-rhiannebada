e8e6209b764f7fe36b1f217804639eb98c334061
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-30 09:47:15 +0400

    Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/exercise_1.py
@@ -0,0 +1,11 @@
+## Exercise 1:  Alien Colors 
+
+print ("Passed version:")
+alien_color = 'green'
+if alien_color == 'green':
+    print ("Player earned 5 points!")
+
+print ("\nFailed version:")
+alien_color = 'yellow'
+if alien_color == 'green':
+    print ("Player earned 5 points!")
