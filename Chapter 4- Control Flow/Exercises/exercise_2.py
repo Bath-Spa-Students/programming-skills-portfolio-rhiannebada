159d6686bf5c9d1e6ca36d8f36b6e98829f7fd6d
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-31 13:39:03 +0400

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise_2.py
@@ -0,0 +1,17 @@
+#Exercise 2: Alien Colors #2 
+
+#Version that runs if block
+print ("Runs the if block:")
+alien_color = 'green'
+if alien_color == 'green':
+    print ("Player earned 5 points!")
+else:
+    print ("Player earned 10 points!")
+
+#Version that runs else block
+print ("\nRuns the else block:")
+alien_color = 'yellow'
+if alien_color == 'green':
+    print ("Player earned 5 points!")
+else:
+    print ("Player earned 10 points!")
