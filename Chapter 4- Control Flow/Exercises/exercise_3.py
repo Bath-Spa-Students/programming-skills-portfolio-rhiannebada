1977cdb090e1982ab1d824b596e6e3dd95403986
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-31 13:39:14 +0400

    Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise_3.py
@@ -0,0 +1,34 @@
+## Exercise 3: Alien Colors #3 
+#Message is printed according to a specific color
+
+#Green version
+print ("Green version:")
+alien_color = 'green'
+if alien_color == 'green':
+    print ("Player earned 5 points!")
+elif alien_color == 'yellow':
+    print ("Player earned 10 points!")
+else:
+    print ("Player earned 15 points!")
+
+
+#Yellow version
+print ("\nYellow version:")
+alien_color = 'yellow'
+if alien_color == 'green':
+    print ("Player earned 5 points!")
+elif alien_color == 'yellow':
+    print ("Player earned 10 points!")
+else:
+    print ("Player earned 15 points!")
+
+
+#Red version
+print ("\nRed version:")
+alien_color = 'red'
+if alien_color == 'green':
+    print ("Player earned 5 points!")
+elif alien_color == 'yellow':
+    print ("Player earned 10 points!")
+else:
+    print ("Player earned 15 points!")
