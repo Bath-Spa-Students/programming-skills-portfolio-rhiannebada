460d4aadda80c64bb633a509d29b200dc52f8e13
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-31 13:39:53 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/exercise_5.py
@@ -0,0 +1,15 @@
+## Exercise 5: Favorite Fruit 
+ 
+#list of my 3 most favorite fruits
+favorite_fruits = ['strawberry', 'mangoes', 'kiwis']
+
+if 'strawberry' in favorite_fruits:
+    print ("You really like strawberries!")
+if 'mangoes' in favorite_fruits:
+    print ("You really like mangoes!")   
+if 'oranges' in favorite_fruits:
+    print ("You really like oranges!") #oranges not in the list, therefore will not print
+if 'apples' in favorite_fruits:
+    print ("You really like apples!") #apples not in the list, therefore will not print
+if 'kiwis' in favorite_fruits:
+    print ("You really like kiwis!")
