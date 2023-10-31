15dab4daf4744a4959da67f5e4d820ee8adb0d26
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-10-31 13:39:24 +0400

    Exercise 4.py

---
+++ Chapter 4- Control Flow/Exercises/exercise_4.py
@@ -0,0 +1,15 @@
+## Exercise 4: Stages of Life
+
+Age = 19 #set a value for variable Age
+if Age<2:
+    print ("You are a baby.")
+elif Age<4:
+    print ("You are a toddler.")
+elif Age<13:
+    print ("You are a kid.")
+elif Age<20:
+    print ("You are a teenager.")
+elif Age<65:
+    print ("You are an adult.")
+else:
+    print ("You are an elder.")
