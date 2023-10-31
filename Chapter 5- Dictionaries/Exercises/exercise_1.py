5a4d4f3c213bfcb1a29d75d208dbc566a8e40e35
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-01 00:18:42 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise_1.py
@@ -0,0 +1,20 @@
+## Exercise 1: Person
+
+#assigning value to key
+person = {'first_name' : 'Jungwon', 
+              'last_name' : 'Yang',
+              'age' : '19',
+              'city' : 'Seoul'}
+
+#print information stored
+print ("First name:")
+print (person ['first_name']) #will display the value Jungwon
+
+print ("\nLast name:")
+print (person ['last_name']) #will display the value Yang
+
+print ("\nAge:")
+print (person ['age']) #will display the value 19
+
+print ("\nCity they live in:")
+print (person ['city']) #will display the value Seoul
