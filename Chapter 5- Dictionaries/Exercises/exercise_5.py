061edfb873d564cc5dd4ebd5be3ebf078a936eb9
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-01 00:19:19 +0400

    Exercise 5.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise_5.py
@@ -0,0 +1,26 @@
+## Exercise 5: Pets
+
+#make several dictionaries including the kind of animal and the owner’s name
+Yeontan = {"specie" : "dog",
+           "breed" : "Pomeranian",
+           "owner" : "Taehyung"}
+
+Layla = {"specie" : "dog",
+         "breed": "Border Collie",
+         "owner" : "Jake"}
+
+Gaeul = {"specie" : "dog",
+         "breed" : "French Poodle",
+         "owner" : "Sunghoon"}
+
+Odi = {"specie" : "hedgehog",
+       "owner" : "Soobin"}
+
+Leon = {"specie" : "cat",
+        "breed" : "Abyssinian",
+        "owner" : "Ten"}
+pets = [Yeontan, Layla, Gaeul, Odi, Leon]
+
+for pet in pets:
+    print (pet)
+
