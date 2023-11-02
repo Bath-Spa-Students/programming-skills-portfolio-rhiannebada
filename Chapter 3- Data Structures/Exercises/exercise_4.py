aee240d58259c2742ba0648007f3413933d714dd
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 18:58:00 +0400

    Exercise 4.py

--- Chapter 3- Data Structures/Exercises/exercise_4.py
+++ Chapter 3- Data Structures/Exercises/exercise_4.py
@@ -1,5 +1,8 @@
 ## Exercise 4: Guest List
 
+#list of people to invite
 names = ["Jewel", "Anne", "JR", "RJ"]
-for i in names:
-    print ("Hi,", i + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [0] + "! Would you like to have dinner with me this coming Tuesday?") #will print a message inviting someone from the list
+print ("Hi,", names [1] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [2] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [3] + "! Would you like to have dinner with me this coming Tuesday?")
