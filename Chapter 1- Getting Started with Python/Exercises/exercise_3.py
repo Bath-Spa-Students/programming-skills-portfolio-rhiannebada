0990ab132c49ad689e2c58b0cf322d5b3f5d9f67
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-03 00:09:44 +0400

    Exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise_3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise_3.py
@@ -1,3 +1,6 @@
+## Exercise 3: Print date and Time
+
+#displays current date and time
 import datetime
 current_time = datetime.datetime.now()
 print ("The current date and time in UAE is: ", current_time)
