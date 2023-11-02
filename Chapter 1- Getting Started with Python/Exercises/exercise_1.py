eae1c3d79d9bca6aa01460dd9acb5b2f206c83c1
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-03 00:09:20 +0400

    Exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise_1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise_1.py
@@ -1,6 +1,9 @@
-print ("""Twinkle, twinkle, little star,
+## Exercise 1: Print Strings 
+
+#print the string following the exact given format
+print ("""Twinkle, twinkle, little star, 
 	How I wonder what you are! 
 		Up above the world so high,   		
 		Like a diamond in the sky. 
 Twinkle, twinkle, little star, 
-	How I wonder what you are""") 
+	How I wonder what you are""") #use of """ to represent multi-line string
