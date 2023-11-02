9799bc64bd08ae87b4de6758eb992cb41e989a76
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 18:58:07 +0400

    Exercise 5.py

--- Chapter 3- Data Structures/Exercises/exercise_5.py
+++ Chapter 3- Data Structures/Exercises/exercise_5.py
@@ -1,17 +1,22 @@
 ## Exercise 5: Change Guest List
 
-guests = ["Jewel", "Anne", "RJ", "JR"]
-for i in guests:
-    print ("Hi,", i + "! Would you like to have dinner with me this coming Tuesday?")
+#list of people to invite
+names = ["Jewel", "Anne", "JR", "RJ"]
+print ("Hi,", names [0] + "! Would you like to have dinner with me this coming Tuesday?") #will print a message inviting someone from the list
+print ("Hi,", names [1] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [2] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [3] + "! Would you like to have dinner with me this coming Tuesday?")
 
-name = guests[3]
-print ("\nSorry,", name, "won't be available that day.\n")
+#one guest can't make it to dinner
+name = names[2]
+print ("\nSorry,", name, "won't be available that day.\n") 
 
 #JR is not available on  Tuesday night
-del (guests[3])
-guests.insert (3, "Eriel")
+del (names[2]) #will delete JR in the list
+names.insert (2, "Eriel") #will be added to the list in replace to JR
 
-#Reprint invitations
-for i in guests:
-    print ("Hi,", i + "! Would you like to have dinner with me this coming Tuesday?")
-
+#reprint invitations
+print ("Hi,", names [0] + "! Would you like to have dinner with me this coming Tuesday?") #will print a message inviting someone from the list
+print ("Hi,", names [1] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [2] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [3] + "! Would you like to have dinner with me this coming Tuesday?")
