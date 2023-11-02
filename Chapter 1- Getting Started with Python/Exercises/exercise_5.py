27b236a536e013da9f2d2ddd0720809e6213a13d
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-03 00:09:59 +0400

    Exercise 5.py

--- Chapter 1- Getting Started with Python/Exercises/exercise_5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise_5.py
@@ -1,6 +1,8 @@
-##Write a Python program which accepts the radius of a circle from the user and compute the area.
-radius = int (input("Enter measurement of radius: "))
-pi = 3.14
-area = pi * radius * radius
+## Exercise 5: Compute area of Circle 
+
+#area of a circle
+radius = float (input("Enter measurement of radius: ")) #will ask input from user; use of float to store decimals
+pi = 3.1416
+area = pi * radius * radius 
 print ("The area of circle is: ", area)
 
