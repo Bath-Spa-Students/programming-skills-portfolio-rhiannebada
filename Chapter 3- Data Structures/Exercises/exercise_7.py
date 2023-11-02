4658f1f881eba560b246f0b2f994d1bd8c092bc4
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 18:58:27 +0400

    Exercise 7.py

--- Chapter 3- Data Structures/Exercises/exercise_7.py
+++ Chapter 3- Data Structures/Exercises/exercise_7.py
@@ -1,36 +1,35 @@
 ## Exercise 7: Seeing the World 
 
-print ("LIST:")
-
+#list of places I want to visit
 places = ['South Korea', 'Palawan', 'Japan', 'Maldives', 'Switzerland']
 
 print ("\nOriginal order:")
-print (places)
+print (places) #will print exact same order as in the list
 
 print ("\nAlphabetical order using sorted():")
-print (sorted(places))
+print (sorted(places)) #will print list in alphabetical order using sorted()
 
 print ("\nOriginal order:")
 print (places)
 
 print ("\nReversed Alphabetical order using sorted():")
-print (sorted(places, reverse=True))
+print (sorted(places, reverse=True)) #will print list in reversed alphabetical order using sorted()
 
 print ("\nOriginal order:")
 print (places)
 
 print ("\nReversed order using reverse():")
-places.reverse()
-print(places)
+places.reverse() #will print list in reversed order using reverse()
+print(places) 
 
 print ("\nOriginal order using reverse():")
-places.reverse()
+places.reverse() #will print exact same order as in the list using reverse()
 print (places)
 
-print ("\nSorted/Alphabetical order using sort():")
-places.sort()
-print (places)
+print ("\nAlphabetical order using sort():")
+places.sort() #will print list in alphabetical order using sort()
+print (places) 
 
 print ("\nReversed Alphabetical order using sort():")
-places.sort(reverse=True)
+places.sort(reverse=True) #will print list in reversed alphabetical order using sort()
 print (places)
