278c0097ae325b817cb7dba084e8ea0330915426
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 18:57:52 +0400

    Exercise 3.py

--- Chapter 3- Data Structures/Exercises/exercise_3.py
+++ Chapter 3- Data Structures/Exercises/exercise_3.py
@@ -1,5 +1,7 @@
 ## Exercise 3: Your Own List
 
+#list of mode of transportation
 transportation = ["car", "bicycle", "scooter"]
-for i in transportation:
-    print ("I prefer using my", i , "to go to school.")
+print ("I prefer using my", transportation [0] , "to go to school.") #print statements using the items
+print ("I prefer using my", transportation [1] , "to go to school.")
+print ("I prefer using my", transportation [2] , "to go to school.")
