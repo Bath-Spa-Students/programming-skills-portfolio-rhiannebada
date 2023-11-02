102da8b62b06e36141766c4510155dbd01c97562
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 19:28:10 +0400

    Exercise 5.py

--- Chapter 2- Variables & Comments/Exercises/exercise_5.py
+++ Chapter 2- Variables & Comments/Exercises/exercise_5.py
@@ -1,9 +1,14 @@
-#Exercise 5: USB Shopper : A girl heads to a computer shop to buy some USB sticks. 
-#She loves USB sticks and wants as many as she can get for £50. They are £6 each. Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left. You will to use the arithmetic operators to complete this exercise.
+## Exercise 5: USB Shopper
 
+#calculation using division
 cash = 50
 price = 6
-quantity = cash//price
-print (f"She can buy", quantity, "USB sticks.")
+quantity = cash//price #use of // to get result as whole number
+
+#calculate how many USB sticks can be bought
+print (f"She can buy", quantity, "USB sticks.") #use of f-string for formatted string literals
+
+#calculate how much money will be left
 change = cash%price
+
 print (f"She will have a change of £", change, ".")
