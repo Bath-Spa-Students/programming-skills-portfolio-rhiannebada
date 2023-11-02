57e49f9c32495af97d6bcf09104efd23b49dfa86
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 18:57:45 +0400

    Exercise 2.py

--- Chapter 3- Data Structures/Exercises/exercise_2.py
+++ Chapter 3- Data Structures/Exercises/exercise_2.py
@@ -1,5 +1,8 @@
 ## Exercise 2: Greetings
 
+#list of names of friends
 names = ["Jewel", "Anne", "JR", "RJ"]
-for i in names:
-    print ("Good morning,", i + "!")
+print ("Good morning,", names [0] + "!") #print a message for each element, one by one
+print ("Good morning,", names [1] + "!")
+print ("Good morning,", names [2] + "!")
+print ("Good morning,", names [3] + "!")
