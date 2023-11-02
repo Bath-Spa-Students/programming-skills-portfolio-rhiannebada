4638418975651403accb75b60aa8bac3a7f06fc8
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 18:58:13 +0400

    Exercise 6.py

--- Chapter 3- Data Structures/Exercises/exercise_6.py
+++ Chapter 3- Data Structures/Exercises/exercise_6.py
@@ -1,33 +1,41 @@
 ## Exercise 6: Shrinking Guest List 
 
-guests = ["Jewel", "Anne", "JR"]
-for i in guests:
-    print ("Hi,", i + "! Would you like to have dinner with me this coming Tuesday?")
+#list of people to invite
+names = ["Jewel", "Anne", "JR", "RJ"]
+print ("Hi,", names [0] + "! Would you like to have dinner with me this coming Tuesday?") #will print a message inviting someone from the list
+print ("Hi,", names [1] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [2] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [3] + "! Would you like to have dinner with me this coming Tuesday?")
 
-name = guests[2]
-print ("\nSorry,", name, "won't be available that day.\n")
+#one guest can't make it to dinner
+name = names[2]
+print ("\nSorry,", name, "won't be available that day.\n") 
 
-#JR is not available on Tuesday night
-del (guests[2])
-guests.insert (2, "Eriel")
+#JR is not available on  Tuesday night
+del (names[2]) #will delete JR in the list
+names.insert (2, "Eriel") #will be added to the list in replace to JR
 
-#Reprint invitations
-for i in guests:
-    print ("Hi,", i + "! Would you like to have dinner with me this coming Tuesday?")
+#reprint invitations
+print ("Hi,", names [0] + "! Would you like to have dinner with me this coming Tuesday?") #will print a message inviting someone from the list
+print ("Hi,", names [1] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [2] + "! Would you like to have dinner with me this coming Tuesday?")
+print ("Hi,", names [3] + "! Would you like to have dinner with me this coming Tuesday?")
 
 #Table won't arrive on time; only have space for two people
 print ("\nMy apologies, I can only invite two people for dinner.")
-name = guests.pop()
-print ("My apologies,", name, "there's no more seats left at the table.\n")
+name = names.pop() #will remove guest from the list
+print ("\tMy apologies,", name, "there's no more seats left at the table.") #print an apology message
+name = names.pop()
+print ("\tMy apologies,", name, "there's no more seats left at the table.\n")
 
-#Invite two people
-name = guests [0]
+#Invite remaining two guest in the list
+name = names [0] 
 print (name + ", please come to dinner.")
-name = guests [1]
+name = names [1]
 print (name + ", please come to dinner.\n")
 
 #Empty list
-del (guests [0])
-del (guests [0])
+del (names [0])
+del (names [0])
 
-print (guests)
+print (names) #to check if the list is empty
