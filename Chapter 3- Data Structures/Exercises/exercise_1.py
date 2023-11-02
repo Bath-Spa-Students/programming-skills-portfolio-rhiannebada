7cc6070c4e4e140775d0c09bf14293c67850c3c5
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-02 18:57:38 +0400

    Exercise 1.py

--- Chapter 3- Data Structures/Exercises/exercise_1.py
+++ Chapter 3- Data Structures/Exercises/exercise_1.py
@@ -1,7 +1,8 @@
 ## Exercise 1: Names
 
+#list of names of friends
 names = ["Jewel", "Anne", "JR", "RJ"]
-print (names [0])
+print (names [0]) #assessing each element one by one
 print (names [1])
 print (names [2])
 print (names [3])
