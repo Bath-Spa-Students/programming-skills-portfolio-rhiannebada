bb554cd90a1b803f9900a7e4ac6c8d08ccb86e83
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-03 21:19:10 +0400

    Practice 4.py

---
+++ Chapter 2- Variables & Comments/Practices/practice_4.py
@@ -0,0 +1,7 @@
+#store an integer and string in variables x and y
+x = 7
+y = "Enhypen"
+
+#print the type of variable x and y
+print ("Type of variable x:", type(x))
+print ("Type of variable y:", type(y))
