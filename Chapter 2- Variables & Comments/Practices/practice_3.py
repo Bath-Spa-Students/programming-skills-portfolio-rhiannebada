3ada6f8d8d0bcc5c6566e30619cee71145ea9a8e
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-03 21:19:05 +0400

    Practice 3.py

---
+++ Chapter 2- Variables & Comments/Practices/practice_3.py
@@ -0,0 +1,18 @@
+#typecast input to string, int, and float
+
+#take input from user
+number = input("Enter a number: ")
+
+#typecast to string
+string_num = str(number)
+
+#typecast to integer
+int_num = int(number)
+
+#typecast to float
+float_num = float(number)
+
+#print all variables
+print ("\nString variable:", string_num)
+print ("Integer variable:", int_num)
+print ("Float variable:", float_num)
