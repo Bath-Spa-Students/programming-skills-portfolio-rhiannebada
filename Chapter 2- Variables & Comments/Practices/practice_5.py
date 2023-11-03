10adf7b37640e205e59c80794949ae9491218fad
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-03 21:19:18 +0400

    Practice 5.py

---
+++ Chapter 2- Variables & Comments/Practices/practice_5.py
@@ -0,0 +1,10 @@
+#store name of fruits in a list
+fruits = ["strawberry", "mango", "kiwi"]
+
+#unpack list into 3 variables x, y and z
+x, y, z = fruits
+
+#print value of x, y and z
+print ("x:", x)
+print ("y:", y)
+print ("z:", z)
