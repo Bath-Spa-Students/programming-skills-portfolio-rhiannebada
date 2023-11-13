e5c842eb735e5f4ac54c05497260a81ea38a556e
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-13 17:56:17 +0400

    Practice 1.py

---
+++ Chapter 3- Data Structures/Practices/practice_1.py
@@ -0,0 +1,8 @@
+## Program that removes all occurrence of an item
+
+#given list of numbers
+list1 = [5, 20, 15, 20, 50, 20]
+
+while 20 in list1:
+    list1.remove (20) #removes all 20 in the list
+print (list1)
