91d4dd4c09c1080df20e041f2640fe779dfcafbd
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-13 11:35:04 +0400

    Exercise 2.py

---
+++ Chapter 7- Functions/Exercises/exercise_2.py
@@ -0,0 +1,8 @@
+## Exercise 2: Favorite Book 
+
+#function that'll print favorith book
+def favorite_book (title):
+
+    print ("One of my favorite books is", title + ".")
+
+favorite_book ("'A Good Girl's Guide to Murder'") #title of the book
