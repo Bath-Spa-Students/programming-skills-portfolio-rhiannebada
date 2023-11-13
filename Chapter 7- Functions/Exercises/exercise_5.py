34aaa442dd33ffaf831658d66be4f815d5844545
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-13 17:56:06 +0400

    Exercise 5.py

--- Chapter 7- Functions/Exercises/exercise_5.py
+++ Chapter 7- Functions/Exercises/exercise_5.py
@@ -5,6 +5,7 @@ def describe_city(city, country='Philippines'):
     sentence = city.title() + " is in " + country.title()+"." #use of title() to display text in capitalisation
     print (sentence)
 
+#3 different cities, 1 is not in default country
 describe_city ('manila')
 describe_city ('cebu')
 describe_city ('seoul', 'south korea')
