885b3fe52ded4a775141c169ee2c0af02a994d36
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-13 22:26:12 +0400

    Practice 3.py

---
+++ Chapter 5- Dictionaries/Practices/practice_3.py
@@ -0,0 +1,10 @@
+## Iterate values in dictionary 
+
+#information in dictionary
+my_info = {'Full Name':'Rhianne Ghernelle Intal Bada',
+           'Age': '19', 'Birthdate': 'October 02, 2004',
+           'Phone Number': '0562052633',
+           'Address': 'Al Nahda, Sharjah, UAE'}
+
+for value in my_info.values():
+    print (value)
