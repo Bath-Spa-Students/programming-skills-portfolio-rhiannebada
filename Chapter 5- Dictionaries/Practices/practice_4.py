bf28b2b32278879421b27728b13c3e12e7dfb495
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-13 22:26:17 +0400

    Practice 4.py

---
+++ Chapter 5- Dictionaries/Practices/practice_4.py
@@ -0,0 +1,10 @@
+## Iterate keys and values in dictionary 
+
+#information in dictionary
+my_info = {'Full Name':'Rhianne Ghernelle Intal Bada',
+           'Age': '19', 'Birthdate': 'October 02, 2004',
+           'Phone Number': '0562052633',
+           'Address': 'Al Nahda, Sharjah, UAE'}
+
+for key, value in my_info.items():
+    print (f"{key}:{value}")
