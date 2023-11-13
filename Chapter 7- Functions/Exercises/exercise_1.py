e776d76be299ea31796aae361b3620d5c252b48e
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-13 11:34:58 +0400

    Exercise 1.py

---
+++ Chapter 7- Functions/Exercises/exercise_1.py
@@ -0,0 +1,9 @@
+## Exercise 1: Message
+
+#function that'll print certain sentence
+def display_message ():
+
+    message = "In this chapter,we are to learn how to store codes in functions."
+    print (message)
+
+display_message ()
