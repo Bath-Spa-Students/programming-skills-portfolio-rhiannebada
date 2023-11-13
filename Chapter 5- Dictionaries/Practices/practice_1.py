d5914d632dff4478908af31997d39c121711e7c9
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-13 22:25:56 +0400

    Practice 1.py

---
+++ Chapter 5- Dictionaries/Practices/practice_1.py
@@ -0,0 +1,7 @@
+## Dictionary
+
+#store information about myself
+my_info = {'Full Name':'Rhianne Ghernelle Intal Bada',
+           'Age': '19', 'Birthdate': 'October 02, 2004',
+           'Phone Number': '0562052633',
+           'Address': 'Al Nahda, Sharjah, UAE'}
