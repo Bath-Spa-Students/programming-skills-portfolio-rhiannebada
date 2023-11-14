2522b0c503dd4094cc643d3bad43cbf8b8fbcb0c
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-15 01:32:13 +0400

    Practice 1.py

---
+++ Chapter 4- Control Flow/Practices/practice_1.py
@@ -0,0 +1,15 @@
+## if statements
+
+#assign values to variables from user input
+x = int(input("Enter a number: "))
+
+if x>100: #if x is greater than 100
+    y=20 #assign 20 to y
+    z=40 #assign 40 to z
+    print ("Variable x is greater than 100.")
+
+    #print values of y and z
+    print(f"y:{y}")
+    print(f"z:{z}")
+
+#no assignment of value to y and z if x is not greater than 100
