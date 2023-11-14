c0a44200ca6c77c2aec94a8def9b5313dfcc10a7
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-15 03:25:23 +0400

    Practice 5.py

--- Chapter 6- Loops/Practices/practice_5.py
+++ Chapter 6- Loops/Practices/practice_5.py
@@ -1,4 +1,4 @@
-##
+## Print largest number from input number/s
 
 largest = 0 #variable to store largest number
 
@@ -8,12 +8,10 @@ while True:
     if number == 0: #checks if input is '0'
         break #exits loop if '0' is entered
 
-    if number > largest: #checks if input number is larger than current largest number
+    if largest==0 or number>largest: #checks if input number is larger than current largest number
         largest = number #updates largest number
-
 #prints largest number 
 if largest != 0:
-    print (f"Largest number entered: {largest}")
-
+    print (f"Largest number entered: {largest}") 
 else:
     print ("No valid number entered.") #if no valid input before '0'
