bdc0ab11b451084cedfbf14d77457156abb83831
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-15 03:16:16 +0400

    Practice 1.py

---
+++ Chapter 6- Loops/Practices/practice_1.py
@@ -0,0 +1,10 @@
+## Print even numbers using 'while' loop
+
+#assign variable with the starting value 
+number = 2
+
+#use of 'while' loop
+while number <= 10: #loop will continue as long as value of 'number' is less than or equal to 10
+    print (number)
+    number += 2 #value is incremented by 2 to get next even number
+                #loop will continue until 'number' exceeds 10
