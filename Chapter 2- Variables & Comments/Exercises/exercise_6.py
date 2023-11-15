35ce58a7df7cfc4243e24046f6ddae07ef8a42cb
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-16 03:21:31 +0400

    Delete exercise_6.py

--- Chapter 2- Variables & Comments/Exercises/exercise_6.py
+++
@@ -1,9 +0,0 @@
-## Exercise 6: Name Cases
-
-#use variable to represent name
-name = "Rhianne"
-
-#print name in different cases
-print ("Name in lowercase:", name.lower()) #display name in all small letters
-print ("Name in uppercase:", name.upper()) #display name in all capital letters
-print ("Name in title case:", name.title()) #change initials to uppercase and the rest lowercase
