4bcecb8821d271e1a3970d48cdda804da779a64a
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-16 06:50:20 +0400

    Exercise 6.py

--- Chapter 2- Variables & Comments/Exercises/newproject_namecases.py
+++ Chapter 2- Variables & Comments/Exercises/newproject_namecases.py
@@ -2,7 +2,7 @@
 
 #use variable to represent name
 name = "Rhianne"
-
+ 
 #print name in different cases
 print ("Name in lowercase:", name.lower()) #display name in all small letters
 print ("Name in uppercase:", name.upper()) #display name in all capital letters
