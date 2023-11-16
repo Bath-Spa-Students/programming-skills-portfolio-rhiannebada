323ba436e1e81ca0be77633beb1d825929c24fed
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-16 09:20:35 +0400

    Exercise 6.py

---
+++ Chapter 6- Loops/Exercises/newproject_rentalcar.py
@@ -0,0 +1,7 @@
+## Exercise 6: Rental Car
+
+#ask user what kind of car they would like to rent
+car = input ("Enter car that you would like to rent: ")
+
+#print a message 
+print ("Let me see if I can find you a", car.title()+".")
