787a3cc81e153e3f21c7fbe39f28a08a768d7827
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2023-11-17 02:35:28 +0400

    Practice 6.py

---
+++ Chapter 6- Loops/Practices/classpractice.py
@@ -0,0 +1,5 @@
+## Activity: Calculate square root of each number in a range
+
+for number in range (1,11): #range from 1-10
+    square_root = number**0.5 #use of exponentation
+    print(f"The square root of {number} is {square_root}.") 
